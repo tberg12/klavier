4896577c4f47fe6693dff73f0e4b967db1c30323
Taylor Berg-Kirkpatrick <tberg@cs.cmu.edu>
2017-04-26 23:21:07 -0400

    add lib

--- klavier.sh
+++ klavier.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx12g -Djava.library.path="./" -jar klavier.jar -inputType FILE -updateActivationsIters 400 -modelNumEMIters 5 -imslpTransModelSer ./data/imslp_indep_transitions.ser -train true -initSpectEnvSer ./data/initSpectEnvFull.ser -spectEnvSer ./data/learnedSpecEnvGould2Itunes.ser -writeTrainedSpect false -filterNoteMinVel 20 -filterNoteMaxIndex 108 -audioFilePath $1
+java -Xmx12g -Djava.library.path="./lib/" -jar klavier.jar -inputType FILE -updateActivationsIters 400 -modelNumEMIters 5 -imslpTransModelSer ./data/imslp_indep_transitions.ser -train true -initSpectEnvSer ./data/initSpectEnvFull.ser -spectEnvSer ./data/learnedSpecEnvGould2Itunes.ser -writeTrainedSpect false -filterNoteMinVel 20 -filterNoteMaxIndex 108 -audioFilePath $1
